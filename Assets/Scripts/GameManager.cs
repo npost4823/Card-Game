f649c37280c70fdbfa1aad812065546275f4d006
WowzaTime <npost4823@student.pps.net>
2026-04-17 13:55:19 -0700

    fixed shuffling

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager gm;
-    public List<Card> deck = new List<Card>();
-    public List<Card> player_deck = new List<Card>();
-    public List<Card> ai_deck = new List<Card>();
-    public List<Card> player_hand = new List<Card>();
-    public List<Card> ai_hand = new List<Card>();
-    public List<Card> discard_pile = new List<Card>();
+    public List<Card_data> deck = new List<Card_data>();
+    public List<Card_data> player_deck = new List<Card_data>();
+    public List<Card_data> ai_deck = new List<Card_data>();
+    public List<Card_data> player_hand = new List<Card_data>();
+    public List<Card_data> ai_hand = new List<Card_data>();
+    public List<Card_data> discard_pile = new List<Card_data>();
+
+    public Card blank;
+    public Canvas canvas;
+    public Transform player_hand_spawnpoint;
+    public Vector3 offset;
+
+
+
+    
 
     private void Awake()
     {
@@ -27,7 +37,8 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        canvas = FindAnyObjectByType<Canvas>();
+        Deal();
     }
 
     // Update is called once per frame
@@ -38,12 +49,35 @@ public class GameManager : MonoBehaviour
 
     void Deal()
     {
-
+        Shuffle(player_deck);
+        Shuffle(ai_deck);
+        for (int i = 0; i < 2; i++)
+        {
+            //player_hand.Add(player_deck[i]);
+            Card current_card = Instantiate(blank, player_hand_spawnpoint.position + offset, Quaternion.identity, canvas.transform);
+            offset.x += 300;
+            current_card.data = player_deck[i];
+            //player_deck.Remove(current_card.data);
+            player_hand.Add(current_card.data);
+            current_card.transform.SetParent(canvas.transform);
+        }
+        for (int i = 0; i < ai_deck.Count; i++)
+        {
+            ai_hand.Add(ai_deck[i]);
+        }
     }
 
-    void Shuffle()
+    void Shuffle(List<Card_data> _deck )
     {
-
+        System.Random rng = new System.Random();
+        for (int i = 0; i < _deck.Count; i++)
+        {
+            int j = rng.Next(_deck.Count);
+            Card_data temp = _deck[i];
+            _deck[i] = _deck[j];
+            _deck[j] = temp;
+        }
+        
     }
 
     void AI_Turn()
